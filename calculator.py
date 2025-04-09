f7a8b1d4821ac26d705aad0bb8596e8f761de367
sofiagalindol <sgalindo@sccs.edu.bo>
2025-04-09 15:44:05 -0400

    Merge remote-tracking branch 'origin/main'

--- calculator.py
+++ calculator.py
@@ -1,38 +1,33 @@
 import math
 
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
 
-One function per operation, in order.
-"""
-# First example
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 
-def add(a, b): 
+def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
+def divide(a, b):
     if a == 0:
         raise ZeroDivisionError
+
     return b/a
 
-def log(a, b):
+def logarithm(a, b):
     if b < 0:
         raise ValueError
     return math.log(b, a)
 
-def exp(a, b):
-    return a**b
-
-
-
-
-
-
+def exponent(a, b):
+    return a ** b
 
